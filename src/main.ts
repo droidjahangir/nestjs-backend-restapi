7c370a326a9f8dd25688c44e7967a2d6ee4ea0a9
droidjahangir <jahangir.droid@gmail.com>
2021-11-17 00:28:29 +0600

    port updated

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,8 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  // const port = process.env.PORT;
-  const port = 3000;
+  const port = process.env.PORT || 3000;
+  // const port = 3000;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
